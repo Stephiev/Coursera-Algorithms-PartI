51ed12e0bc06d681bb9be454310563ea2e194ecc
Stephanie Vasquez-Soltoro <svasquezsoltor@BELC02R3FR4G8WN.sea.corp.expecn.com>
2016-12-28 18:06:18 -0800

    check slopes

--- mergesort/FastCollinearPoints.java
+++ mergesort/FastCollinearPoints.java
@@ -2,15 +2,24 @@ public class FastCollinearPoints {
     
     
 //Think of p as the origin.
-//For each other point q, determine the slope it makes with p.
 //Sort the points according to the slopes they makes with p.
+//For each other point q, determine the slope it makes with p.
 //Check if any 3 (or more) adjacent points in the sorted order have equal slopes with respect to p. If so, these points, together with p, are collinear.
+   
     public FastCollinearPoints(Point[] points) {
-        
-        for (Point point : points) {
+        Point[] sortedPoints = Arrays.copyOf(points, points.length);
         // grab p
-            Point p = point;
-            // sort the array based on slope
+        for (Point p : points) {
+            
+            //Sort the points according to the slopes they makes with p.
+            Arrays.sort(sortedPoints, point.slopeOrder());
+            
+            
+            //For each other point q, determine the slope it makes with p.
+            for (int q = 1; q < sortedPoints.length; q++) { // start at 1 to avoid p
+                // Check slopes
+                
+            }
             
         }
     
